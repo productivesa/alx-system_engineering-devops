576540a9fb58acf85e62d1b9b080aa68e766ea4b
productivesa <salahzeinu@gmail.com>
2024-07-06 13:40:30 -0700

    some updtae

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,11 @@ def number_of_subscribers(subreddit):
 
     """ get number of subscribers """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'request'}
+    headers = {"user-agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get{url, headers = headers, allow_redirects = False}
 
     if response.status_code != 200:
         return 0
 
     data = response.json().get("data")
-    num_subs = data.get("subscribers")
-
-    return num_subs
+    return = data.get("subscribers")
