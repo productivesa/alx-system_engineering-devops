f9904a860410f01221b8f994822b30a2b0bf2080
productivesa <salahzeinu@gmail.com>
2024-07-06 13:52:12 -0700

    tr

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
 
     """ get number of subscribers """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://reddit.com/r/{}/about.json".format(subreddit)
     headers = {"user-agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get{url, headers = headers, allow_redirects = False}
 
